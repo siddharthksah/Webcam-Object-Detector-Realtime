696d6035358dbef45ffb52da861a86a456262525
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2021-02-28 21:11:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # @author Siddharth
 # @website www.siddharthsah.com
-
+import cv2
+import streamlit as st
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 import streamlit as st
 from imutils.video import VideoStream
 from imutils.video import FPS
@@ -8,6 +10,7 @@ import numpy as np
 import imutils
 import time
 import cv2
+
 
 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
@@ -28,10 +31,10 @@ net = cv2.dnn.readNetFromCaffe('MobileNetSSD_deploy.prototxt.txt',
 
 #camera = VideoStream(src=0).start()
 
-camera = cv2.VideoCapture(-1)
-
-time.sleep(2.0)
-fps = FPS().start()
+# camera = cv2.VideoCapture(0)
+#
+# time.sleep(2.0)
+# fps = FPS().start()
 
 favicon = './favicon.png'
 st.set_page_config(page_title='Object Detector WebApp', page_icon = favicon, initial_sidebar_state = 'auto')
@@ -52,77 +55,69 @@ st.image('./header.jpg', use_column_width=True)
 
 
 st.title("Simple Object Detector using Webcam")
-FRAME_WINDOW = st.image([])
+#FRAME_WINDOW = st.image([])
 #camera = cv2.VideoCapture(0)
 
-run = True
+# run = True
+#
+# if st.button('Stop'):
+#     run = False
 
-if st.button('Stop'):
-    run = False
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
 
-while (run):
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = imutils.resize(frame, width=400)
+        img = frame.to_ndarray(format="bgr24")
 
-    # grab the frame dimensions and convert it to a blob
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-                                 0.007843, (300, 300), 127.5)
+        frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        frame = imutils.resize(frame, width=400)
 
-    # pass the blob through the network and obtain the detections and
-    # predictions
-    net.setInput(blob)
-    detections = net.forward()
+        # grab the frame dimensions and convert it to a blob
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+                                     0.007843, (300, 300), 127.5)
 
-    # loop over the detections
-    for i in np.arange(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the prediction
-        confidence = detections[0, 0, i, 2]
+        # pass the blob through the network and obtain the detections and
+        # predictions
+        net.setInput(blob)
+        detections = net.forward()
 
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence
-        if confidence > 0.2:
-            # extract the index of the class label from the
-            # `detections`,
-            idx = int(detections[0, 0, i, 1])
-            # then compute the (x, y)-coordinates of  the bounding box for the object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+        # loop over the detections
+        for i in np.arange(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated with
+            # the prediction
+            confidence = detections[0, 0, i, 2]
 
-            # draw the prediction on the frame
-            label = "{}: {:.2f}%".format(CLASSES[idx],
-                                         confidence * 100)
-            cv2.rectangle(frame, (startX, startY), (endX, endY),
-                          COLORS[idx], 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(frame, label, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+            # filter out weak detections by ensuring the `confidence` is
+            # greater than the minimum confidence
+            if confidence > 0.2:
+                # extract the index of the class label from the
+                # `detections`,
+                idx = int(detections[0, 0, i, 1])
+                # then compute the (x, y)-coordinates of  the bounding box for the object
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
 
-    # show the output frame
-    #cv2.imshow("Frame", imutils.resize(frame, width=500))
-    FRAME_WINDOW.image(frame, use_column_width=True)
-    #key = cv2.waitKey(1) & 0xFF
+                # draw the prediction on the frame
+                label = "{}: {:.2f}%".format(CLASSES[idx],
+                                             confidence * 100)
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                              COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
-    # if the `q` key was pressed, break from the loop
-    # if key == ord("q"):
-    #     break
+        # show the output frame
+        # cv2.imshow("Frame", imutils.resize(frame, width=500))
+        #FRAME_WINDOW.image(frame, use_column_width=True)
 
-    # update the FPS counter
-    fps.update()
+        #img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
 
-# camera.release()
-# cv2.destroyAllWindows()
 
-else:
-    fps.stop()
-    #print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-    #print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+        return frame
 
-    # do a bit of cleanup
-    camera.stop()
-    cv2.destroyAllWindows()
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+
+#st.video(webrtc_streamer(key="example", video_transformer_factory=VideoTransformer))
 
 st.write('\n')
 st.write('\n')
@@ -135,3 +130,4 @@ st.write("""
 # )
 link = '[siddharthsah.com](http://www.siddharthsah.com/)'
 st.markdown(link, unsafe_allow_html=True)
+
